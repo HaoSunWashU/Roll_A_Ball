c281849c863d10d76665d5d7422aa9248d2b8c68
Hao Sun <hao.sun@wustl.edu>
2018-02-08 10:33:19 -0600

    finish project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position; //relative position between camera and player
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () { //call after other object update
+		transform.position = player.transform.position + offset; 
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public Text countText;
+	public Text winText;
+	public float speed;
+	private int count;
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+		
+	//update and fixedUpdate change the object each frame
+	//update() is called before rendering a frame
+	void Update()
+	{
+
+	}
+
+	//FixedUpdate() is called just before performing any physics caluculations
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		//access other components in this object
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+//		Destroy(other.gameObject);
+//		if (other.gameObject.CompareTag("Player"))
+//		gameObject.SetActive(false);
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+
+	}
+
+	void SetCountText()
+	{	
+		countText.text = "Count: " + count.ToString();
+		if (count >= 13)
+		{
+			winText.text = "You WIN !!!";
+		}
+	}
+
+}
